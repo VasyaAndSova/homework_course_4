1554914b72e5b17a835d4fbe8ad81aa07c32a95e
Василиса Андросова <and3vasya@yandex.ru>
2024-09-17 19:53:31 +0300

    homework with mod done

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from src.product import Product
 from src.utils import create_objects_from_json, read_json
 
 if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product1 = Product("Samsung Galaxy S23 Ultra",
+                       "256GB, Серый цвет, 200MP камера",
+                       180000.0,
+                       5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
@@ -24,7 +27,8 @@ if __name__ == "__main__":
 
     category1 = Category(
         "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        "Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
 
@@ -37,7 +41,8 @@ if __name__ == "__main__":
     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
     category2 = Category(
         "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        "Современный телевизор, который позволяет наслаждаться просмотром, "
+        "станет вашим другом и помощником",
         [product4],
     )
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -35,4 +35,7 @@ def second_category():
 
 @pytest.fixture
 def product():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product("Samsung Galaxy S23 Ultra",
+                   "256GB, Серый цвет, 200MP камера",
+                   180000.0,
+                   5)
