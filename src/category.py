a08d85256fd1a69c941a9e343037baf1419e70e8
Василиса Андросова <and3vasya@yandex.ru>
2024-09-23 23:11:50 +0300

    main tasks done

--- src/category.py
+++ src/category.py
@@ -8,6 +8,22 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def add_product(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+        return product_str
+
+    @add_product.setter
+    def add_product(self, task):
+        self.__products.append(task)
+        Category.product_count += 1
+
+    @property
+    def products_in_list(self):
+        return self.__products
