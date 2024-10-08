be87faef80794f4803ae3b1bd2d2508ce54adf36
Василиса Андросова <and3vasya@yandex.ru>
2024-10-08 16:53:40 +0300

    homework 16.2 done

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.category import Category
-from src.lawngrass import LawnGrass
+# from src.lawngrass import LawnGrass
 from src.product import Product
-from src.product_iterator import ProductIterator
-from src.smartphone import Smartphone
-from src.utils import create_objects_from_json, read_json
+# from src.product_iterator import ProductIterator
+# from src.smartphone import Smartphone
+# from src.utils import create_objects_from_json, read_json
 
 # if __name__ == "__main__":
 #     product1 = Product("Samsung Galaxy S23 Ultra",
@@ -202,7 +202,8 @@ from src.utils import create_objects_from_json, read_json
 #         Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"),
 #     ]
 #
-#     # check_class = [LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый"),
+#     # check_class = [LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20,
+#     "Россия", "7 дней", "Зеленый"),
 #     #                Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"),
 #     #                Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])]
 #
@@ -213,8 +214,9 @@ from src.utils import create_objects_from_json, read_json
 #             raise TypeError
 #         print(new_list.products)
 
-if __name__ == '__main__':
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
+                       180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
@@ -233,9 +235,12 @@ if __name__ == '__main__':
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -243,10 +248,12 @@ if __name__ == '__main__':
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
     print(category2.name)
     print(category2.description)
